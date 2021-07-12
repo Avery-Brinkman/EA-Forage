b7bcc9d909543599c29fad28ab84309bd8c3c40a
Avery Brinkman <averybrinkman@gmail.com>
2021-07-12 00:18:43 -0400

    Rewriting code

--- PythonTasks/VaxMan.py
+++ PythonTasks/VaxMan.py
@@ -1,192 +1,168 @@
-# ORIGINAL CODE TAKEN FROM GRANT JENKS FREE PYTHON GAMES
+# 'tiles' GRID TAKEN FROM GRANT JENKS FREE PYTHON GAMES
 # https://github.com/grantjenks/free-python-games/blob/master/freegames/pacman.py
 
-"""Pacman, classic arcade game.
-Exercises
-1. Change the board.
-2. Change the number of ghosts.
-3. Change where pacman starts.
-4. Make the ghosts faster/slower.
-5. Make the ghosts smarter.
-"""
+import pygame
+from pygame import surface
+from pygame.constants import *
+from numpy import array
 
-from random import choice
-from turtle import *
-
-from freegames import floor, vector
-
-state = {'score': 0}
-path = Turtle(visible=False)
-writer = Turtle(visible=False)
-aim = vector(5, 0)
-pacman = vector(-40, -80)
-
-class ghost:
-    def __init__(self, point, course):
-        self.point = vector(point.x, point.y)
-        self.course = vector(course.x, course.y)
-        self.isVaccinated = False
-    
-    def vaccinate(self):
-        self.isVaccinated = True
-
-ghostList = [
-    ghost(vector(-180, 160), vector(5, 0)),
-    ghost(vector(-180, -160), vector(0, 5)),
-    ghost(vector(100, 160), vector(0, -5)),
-    ghost(vector(100, -160), vector(-5, 0)),
-]
+pygame.init()
 
 # fmt: off
 tiles = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 # fmt: on
 
+# Scale = 420x420 (Grid size 20)
+DISP_W = 420
+DISP_H = 420
+GRID_SIZE = 20
 
-def square(x, y):
-    "Draw square using path at (x, y)."
-    path.up()
-    path.goto(x, y)
-    path.down()
-    path.begin_fill()
+# Colors
+RED =       (255, 0,   0)
+GREEN =     (0,   255, 0)
+BLUE =      (0,   0,   255)
+YELLOW =    (255, 255, 0)
+WHITE =     (255, 255, 255)
+BLACK =     (0,   0,   0)
+BACKGROUND_COLOR = BLUE
 
-    for count in range(4):
-        path.forward(20)
-        path.left(90)
+# Creating Screen
+screen = pygame.display.set_mode((DISP_W, DISP_H))
+pygame.display.update()
+pygame.display.set_caption('VaxMan')
 
-    path.end_fill()
+# Pygame clock
+clock = pygame.time.Clock()
 
+maze = pygame.sprite.Group()
+bricks = pygame.sprite.Group()
+pills = pygame.sprite.Group()
 
-def offset(point):
-    "Return offset of point in tiles."
-    x = (floor(point.x, 20) + 200) / 20
-    y = (180 - floor(point.y, 20)) / 20
-    index = int(x + y * 20)
-    return index
+# Maze drawing
+def createMaze():
+    mY = 0
+    for row in tiles:
+        mX = 0
+        for square in row:
+            if square == 0:
+                Brick(mX, mY, GRID_SIZE, GRID_SIZE)
+            mX += GRID_SIZE
+        mY += GRID_SIZE    
 
+# Brick class
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, surf = screen, color = BLACK):
+        pygame.sprite.Sprite.__init__(self, bricks)
+        
+        self.surf = surf 
+        self.color = color
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.rect = (self.x, self.y, self.w, self.h)
+        self.pos = (x, y)
 
-def valid(point):
-    "Return True if point is valid in tiles."
-    index = offset(point)
+    def draw(self):
+        pygame.draw.rect(self.surf, self.color, self.rect)
 
-    if tiles[index] == 0:
-        return False
+# Movement shortcuts 
+SPEED = 1
+UP = array([0, -1])
+DOWN = array([0, 1])
+LEFT = array([-1, 0])
+RIGHT = array([1, 0])
+STOPPED = array([0, 0])
 
-    index = offset(point + 19)
+# Player
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.pos = array([x, y])
+        self.dir = STOPPED
+        self.intPos = (x, y)
+        self.r = GRID_SIZE / 2
 
-    if tiles[index] == 0:
-        return False
+        self.rect = pygame.draw.circle(screen, YELLOW, self.pos, self.r)
 
-    return point.x % 20 == 0 or point.y % 20 == 0
-
-
-def world():
-    "Draw world using path."
-    bgcolor('black')
-    path.color('blue')
-
-    for index in range(len(tiles)):
-        tile = tiles[index]
-
-        if tile > 0:
-            x = (index % 20) * 20 - 200
-            y = 180 - (index // 20) * 20
-            square(x, y)
-
-            if tile == 1:
-                path.up()
-                path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
-
-
-def move():
-    "Move pacman and all ghosts."
-    writer.undo()
-    writer.write(state['score'])
-
-    clear()
-
-    if valid(pacman + aim):
-        pacman.move(aim)
-
-    index = offset(pacman)
-
-    if tiles[index] == 1:
-        tiles[index] = 2
-        state['score'] += 1
-        x = (index % 20) * 20 - 200
-        y = 180 - (index // 20) * 20
-        square(x, y)
-
-    up()
-    goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')
-
-    for ghost in ghostList:
-        if valid(ghost.point + ghost.course):
-            ghost.point.move(ghost.course)
+    def getDir(self, key):
+        if key == K_UP:
+            return UP * SPEED
+        elif key == K_DOWN:
+            return DOWN * SPEED
+        elif key == K_LEFT:
+            return LEFT * SPEED
+        elif key == K_RIGHT:
+            return RIGHT * SPEED
         else:
-            options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
-            ]
-            plan = choice(options)
-            ghost.course.x = plan.x
-            ghost.course.y = plan.y
+            return self.dir        
 
-        up()
-        goto(ghost.point.x + 10, ghost.point.y + 10)
-        dot(20, 'green') if ghost.isVaccinated else dot(20, 'red')
+    def input(self, key):
+        self.dir = self.getDir(key)
+        
+    def collision(self, dir):
+        origCenter = self.pos
+        tstCenter = origCenter + dir
+        tstPnt = tstCenter + (dir * self.r)
+        
+        for b in bricks:
+            if (tstPnt[0] in range(b.x, b.x + b.w)) and (tstPnt[1] in range(b.y, b.y + b.h)):
+                return True
+        return False
 
-    update()
+    def move(self):       
+        self.pos += self.dir
+        # self.dir = array([0, 0])
+        self.rect = pygame.draw.circle(screen, YELLOW, self.pos, self.r)
 
-    for ghost in ghostList:
-        if abs(pacman - ghost.point) < 20:
-            ghost.vaccinate()
+player = Player(50, 30)
+createMaze()
 
-    ontimer(move, 100)
+# print(f'{bricks.sprites()}')
+playing = True
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                playing = False
+            print(pygame.key.name(event.key))
+            if not player.collision(player.getDir(event.key)):
+                player.input(event.key)
 
+    if player.collision(player.dir):
+        player.dir = STOPPED
+    screen.fill(BACKGROUND_COLOR)
+    for b in bricks:
+        b.draw()
+    player.move()
+    pygame.display.update()
 
-def change(x, y):
-    "Change pacman aim if valid."
-    if valid(pacman + vector(x, y)):
-        aim.x = x
-        aim.y = y
+    clock.tick(30)
 
-
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-writer.goto(160, 160)
-writer.color('white')
-writer.write(state['score'])
-listen()
-onkey(lambda: change(5, 0), 'Right')
-onkey(lambda: change(-5, 0), 'Left')
-onkey(lambda: change(0, 5), 'Up')
-onkey(lambda: change(0, -5), 'Down')
-world()
-move()
-done()
+pygame.quit()
+quit()
