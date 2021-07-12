69dd77a607a0d13afc20f9810b91711a715a8851
Avery Brinkman <averybrinkman@gmail.com>
2021-07-12 01:48:57 -0400

    Ghosts, dots, better movement, collision

--- PythonTasks/VaxMan.py
+++ PythonTasks/VaxMan.py
@@ -2,41 +2,42 @@
 # https://github.com/grantjenks/free-python-games/blob/master/freegames/pacman.py
 
 import pygame
-from pygame import surface
+from random import choice
 from pygame.constants import *
 from numpy import array
 
 pygame.init()
 
+# Score tracking
+Score = 0
+
 # fmt: off
 tiles = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 # fmt: on
 
 # Scale = 420x420 (Grid size 20)
-DISP_W = 420
-DISP_H = 420
+DISP_W = 340
+DISP_H = 380
 GRID_SIZE = 20
 
 # Colors
@@ -56,9 +57,10 @@ pygame.display.set_caption('VaxMan')
 # Pygame clock
 clock = pygame.time.Clock()
 
-maze = pygame.sprite.Group()
+# Sprite groups
 bricks = pygame.sprite.Group()
-pills = pygame.sprite.Group()
+dots = pygame.sprite.Group()
+ghosts = pygame.sprite.Group()
 
 # Maze drawing
 def createMaze():
@@ -68,8 +70,31 @@ def createMaze():
         for square in row:
             if square == 0:
                 Brick(mX, mY, GRID_SIZE, GRID_SIZE)
+            elif square == 1:
+                Dot(mX + GRID_SIZE / 2, mY + GRID_SIZE / 2)
             mX += GRID_SIZE
         mY += GRID_SIZE    
+
+# Round to nearest multiple (https://stackoverflow.com/a/2272174)
+def roundNear(pos, base = 10):
+    xVal = int(base * round(int(pos[0]) / base))
+    yVal = int(base * round(int(pos[1]) / base))
+    return array([xVal, yVal])
+
+class Dot(pygame.sprite.Sprite):
+    def __init__(self, x, y, surf = screen, color = WHITE):
+        pygame.sprite.Sprite.__init__(self, dots)
+
+        self.r = 2
+
+        self.surf = surf
+        self.color = color
+        self.x = x
+        self.y = y
+        self.rect = pygame.draw.circle(self.surf, self.color, (self.x, self.y), self.r)
+
+    def draw(self):
+        pygame.draw.circle(self.surf, self.color, (self.x, self.y), self.r)
 
 # Brick class
 class Brick(pygame.sprite.Sprite):
@@ -83,7 +108,6 @@ class Brick(pygame.sprite.Sprite):
         self.w = w
         self.h = h
         self.rect = (self.x, self.y, self.w, self.h)
-        self.pos = (x, y)
 
     def draw(self):
         pygame.draw.rect(self.surf, self.color, self.rect)
@@ -95,18 +119,18 @@ DOWN = array([0, 1])
 LEFT = array([-1, 0])
 RIGHT = array([1, 0])
 STOPPED = array([0, 0])
+MOVESET = [UP, DOWN, LEFT, RIGHT]
 
 # Player
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, color = YELLOW):
         pygame.sprite.Sprite.__init__(self)
         
+        self.color = color
         self.pos = array([x, y])
         self.dir = STOPPED
-        self.intPos = (x, y)
         self.r = GRID_SIZE / 2
-
-        self.rect = pygame.draw.circle(screen, YELLOW, self.pos, self.r)
+        self.rect = pygame.draw.circle(screen, self.color, self.pos, self.r)
 
     def getDir(self, key):
         if key == K_UP:
@@ -121,6 +145,8 @@ class Player(pygame.sprite.Sprite):
             return self.dir        
 
     def input(self, key):
+        if ((self.dir != self.getDir(key)).all() and (self.dir != -1 * self.getDir(key)).all()): 
+            self.pos = roundNear(self.pos)
         self.dir = self.getDir(key)
         
     def collision(self, dir):
@@ -135,13 +161,48 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):       
         self.pos += self.dir
-        # self.dir = array([0, 0])
-        self.rect = pygame.draw.circle(screen, YELLOW, self.pos, self.r)
+        self.rect = pygame.draw.circle(screen, self.color, self.pos, self.r)
 
-player = Player(50, 30)
+# Ghost class
+class Ghost(pygame.sprite.Sprite):
+    def __init__(self, x, y, color = YELLOW):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.color = color
+        self.pos = array([x, y])
+        self.dir = choice(MOVESET)
+        self.r = GRID_SIZE / 2
+        self.rect = pygame.draw.circle(screen, self.color, self.pos, self.r)
+   
+    def changeDir(self):
+        newDir = choice(MOVESET)
+        while (self.dir == newDir).all() or (self.dir == -1 * newDir).all() or self.collision(newDir):
+            newDir = choice(MOVESET)
+        self.dir = newDir
+        
+    def collision(self, dir):
+        origCenter = self.pos
+        tstCenter = origCenter + dir
+        tstPnt = tstCenter + (dir * self.r)
+        
+        for b in bricks:
+            if (tstPnt[0] in range(b.x, b.x + b.w)) and (tstPnt[1] in range(b.y, b.y + b.h)):
+                return True
+        return False
+
+    def move(self):       
+        self.pos += self.dir * SPEED
+        self.rect = pygame.draw.circle(screen, self.color, self.pos, self.r)
+
+# Adding initial ghosts
+ghosts.add(Ghost(30, 30, RED))
+ghosts.add(Ghost(310, 30, RED))
+ghosts.add(Ghost(30, 350, RED))
+ghosts.add(Ghost(310, 350, RED))
+
+player = Player(DISP_W / 2, DISP_H / 2)
 createMaze()
 
-# print(f'{bricks.sprites()}')
 playing = True
 while playing:
     for event in pygame.event.get():
@@ -156,13 +217,32 @@ while playing:
 
     if player.collision(player.dir):
         player.dir = STOPPED
+
+    # Dot collection
+    for i in pygame.sprite.spritecollide(player, dots, True):
+        Score += 10
+        print(Score)
+
+    # Vaccination handler
+    for i in pygame.sprite.spritecollide(player, ghosts, True):
+        Score += 50
+        print(Score)
+
     screen.fill(BACKGROUND_COLOR)
     for b in bricks:
         b.draw()
+    for d in dots:
+        d.draw()
+    for g in ghosts:
+        if g.collision(g.dir):
+            g.changeDir()
+        
+        print(f'{g = } | {g.dir = }')
+        g.move()
     player.move()
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(60)
 
 pygame.quit()
 quit()
